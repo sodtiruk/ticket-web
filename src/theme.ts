427682d56041e00b0e0128c9cdd8b5c44604e1dd
sodtiruk <sodtiruk22@gmail.com>
2025-02-01 11:36:49 +0700

    fix: change font to mali

--- src/theme.ts
+++ src/theme.ts
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
     typography: {
-        fontFamily: 'Kanit',
+        fontFamily: 'Mali, sans-serif',
     },
 });
 
