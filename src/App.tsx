89ffb117e9f770e810ad9eaeef33b6dfe2d82113
sodtiruk <sodtiruk22@gmail.com>
2025-02-01 11:13:36 +0700

    feat: add theme react and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from '@mui/material/styles';
 import './App.css'
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
+import { theme } from './theme';
 
 function App() {
 
   return (
-    <Router basename='/app'>
-      <AppRoutes />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router basename='/app'>
+        <AppRoutes />
+      </Router>
+    </ThemeProvider>
   )
 }
 
