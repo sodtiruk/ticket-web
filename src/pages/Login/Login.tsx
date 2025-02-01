63105ceb1b818a944695b13a1f79191f45f5132d
sodtiruk <sodtiruk22@gmail.com>
2025-02-01 11:14:04 +0700

    refactor: add <></> in component

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -15,57 +15,63 @@ const Login: React.FC = () => {
     };
 
     return (
-        <Box
-            className="flex justify-center items-center h-screen bg-gray-100"
-        >
-            <Paper
-                elevation={3}
-                className="w-96 p-8"
+        <>
+            <Box
+                className="flex justify-center items-center h-screen bg-gray-100"
             >
-                <Typography variant="h4" className="text-center mb-6">
-                    เข้าสู่ระบบ 
-                </Typography>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <TextField
+                <Paper
+                    elevation={3}
+                    className="w-96 p-8"
+                >
+                    <Typography variant="h4" className="text-center mb-6">
+                        เข้าสู่ระบบ
+                    </Typography>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <TextField
+                                fullWidth
+                                label="Email"
+                                variant="outlined"
+                                type="email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <TextField
+                                fullWidth
+                                label="Password"
+                                variant="outlined"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <Button
                             fullWidth
-                            label="Email"
-                            variant="outlined"
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <TextField
-                            fullWidth
-                            label="Password"
-                            variant="outlined"
-                            type="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
-                    <Button
-                        fullWidth
-                        type="submit"
-                        variant="contained"
-                        color="primary"
-                        className="mb-4"
-                    >
-                        Login
-                    </Button>
-                    <div className="text-center">
-                        <Typography variant="body2">
-                            Don't have an account?{' '}
-                            <a href="#" className="text-blue-500 hover:underline">
-                                Sign up
-                            </a>
-                        </Typography>
-                    </div>
-                </form>
-            </Paper>
-        </Box>
+                            type="submit"
+                            variant="contained"
+                            color="primary"
+                            className="mb-4"
+                        >
+                            Login
+                        </Button>
+                        <div className="text-center">
+                            <Typography variant="body2">
+                                Don't have an account?{' '}
+                                <a href="#" className="text-blue-500 hover:underline">
+                                    Sign up
+                                </a>
+                            </Typography>
+                        </div>
+                    </form>
+                </Paper>
+            </Box>
+
+            <div>
+                Hello world
+            </div>
+        </>
     )
 }
 
